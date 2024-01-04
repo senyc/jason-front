8a8dc3945b0644159430601ab747d1b140868539
senyc <95313103+senyc@users.noreply.github.com>
2024-01-04 14:23:04 -0500

    feat: update header links highlighting

--- src/lib/components/header.tsx
+++ src/lib/components/header.tsx
@@ -1,29 +1,40 @@
 import HeaderLink from "@components/headerLink";
 import Image from "next/image";
 import Logo from "../../../public/jasonLogo.png";
-import LogoMenu from "./logoMenu";
+import ProfileDropdown from "./profileDropdown";
 export default function Header() {
   return (
-    <nav className="bg-base-200 mb-8 h-12 w-full flex-row items-center">
-      <div className="mx-auto flex h-full w-6/12 flex-row items-center justify-start gap-10">
-        <Image quality={100} width="35" src={Logo} alt="" />
+    <nav className="mb-8 h-14 w-full flex-row items-center border-b-[.1px] border-gray-200">
+      <ul className="mx-auto flex h-full w-6/12 flex-row items-center justify-start gap-10">
+        <li>
+          <Image quality={100} width="35" src={Logo} alt="" />
+        </li>
         <div className="w-12" />
-        <HeaderLink
-          href="docs"
-          label="Docs"
-        />
-        <HeaderLink
-          href="blog"
-          label="Blog"
-        />
-        <HeaderLink
-          href="tasks"
-          label="Tasks"
-        />
-        <div className="ml-auto">
-          <LogoMenu />
-        </div>
-      </div>
+        <li>
+          <HeaderLink
+            href="docs"
+            label="Docs"
+            size='sm'
+          />
+        </li>
+        <li>
+          <HeaderLink
+            href="blog"
+            label="Blog"
+            size='sm'
+          />
+        </li>
+        <li>
+          <HeaderLink
+            href="tasks"
+            label="Tasks"
+            size='sm'
+          />
+        </li>
+        <li className="ml-auto self-center">
+          <ProfileDropdown />
+        </li>
+      </ul>
     </nav >
 
   );
--- src/lib/components/headerLink.tsx
+++ src/lib/components/headerLink.tsx
@@ -6,14 +6,13 @@ interface HeaderLinkProps {
   label: string;
   openInNewTab?: boolean;
   className?: string;
-  underline?: boolean;
   onClick?: () => void;
 }
-export default function HeaderLink({ size = 'lg', href, label, openInNewTab = false, className = '', underline = true, onClick}: HeaderLinkProps) {
+export default function HeaderLink({ size = 'lg', href, label, openInNewTab = false, className = '',  onClick}: HeaderLinkProps) {
   return (
     <>
       <Link
-        className={`text-${size} ${ underline && 'hover:underline'} ${className}`.replace('false','')}
+        className={`text-${size} ${className} duration-150 transition hover:text-gray-900`}
         href={href}
         target={openInNewTab ? '_blank' : '_self'}
         onClick={onClick}
