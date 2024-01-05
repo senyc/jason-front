c7f4b6d44cefb579c8bd5c8680bc33c86a59c38c
senyc <95313103+senyc@users.noreply.github.com>
2024-01-05 10:55:11 -0500

    feat: force items without due dates to be displayed on the bottom

--- src/lib/components/taskView.tsx
+++ src/lib/components/taskView.tsx
@@ -1,11 +1,17 @@
 import Task from "@annotations/task";
 import TaskDisplay from "./taskDisplay";
-import { Priority } from "../annotations/priority";
+import { Priority } from "@annotations/priority";
 
 
 type DatedTasks = Map<string, Task[]>;
 const renderTasks = (datedTasks: DatedTasks) => {
   const elements = [];
+  // Puts items without a due date last (instead of first)
+  if (datedTasks.has("")) {
+    const nullItems = datedTasks.get("") as Task[];
+    datedTasks.delete("");
+    datedTasks.set("", nullItems);
+  }
 
   for (const [key, tasks] of datedTasks) {
     elements.push(
@@ -45,6 +51,7 @@ export default function TaskView({ tasks }: TaskViewProps) {
   let datedTasks: DatedTasks = new Map();
 
   tasks.forEach(task => {
+    // Empty string as the key for empty due dates
     if (!task.due) {
       const newElement = datedTasks.get("") || [];
       newElement.push(task);
@@ -60,5 +67,4 @@ export default function TaskView({ tasks }: TaskViewProps) {
       {renderTasks(datedTasks)}
     </ul>
   );
-
 };
