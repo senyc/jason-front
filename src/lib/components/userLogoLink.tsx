2b5e52b5558e5ce153ae67f0f2483750cd759513
senyc <95313103+senyc@users.noreply.github.com>
2024-01-08 10:33:01 -0500

    feat: make profile links clickable on entire div

--- src/lib/components/userLogoLink.tsx
+++ src/lib/components/userLogoLink.tsx
@@ -8,13 +8,13 @@ interface UserLogoLinkProps {
 }
 export default function UserLogoLink({ label, href, icon, highlight = true }: UserLogoLinkProps) {
   return (
-    <li className={`flex h-full w-full flex-row items-center justify-between px-4 py-1 transition duration-75 ease-in ${!!highlight && "hover:bg-gray-100"} hover:text-gray-900`}>
+    <li className={`h-full w-full transition duration-75 ease-in ${!!highlight && "hover:bg-gray-100"} hover:text-gray-900`}>
       <a
-        className="text-sm"
+        className="flex flex-row items-center justify-between px-4 py-1 text-sm"
         href={href}
       >{label}
+        {icon}
       </a>
-      {icon}
     </li >
   );
 }
